a8b528d3106ddad4f754f3cfe02fedaeee4abd7f
Zachary Watkins <watkinza@gmail.com>
2018-06-25 13:30:29 -0500

    Replace search form with list of keywords that direct the user to a page of all custom posts using that link

--- view/search-majors-degrees.php
+++ view/search-majors-degrees.php
@@ -1,15 +1,39 @@
 <?php
 	$search_terms = get_search_query();
-?>
 
+	$mjdq = new WP_Query(array(
+		'post_type' => 'majors-and-degrees'
+	));
+	$mjdids = array();
+
+	foreach ($mjdq->posts as $key => $value) {
+		$mjdids[] = $value->ID;
+	}
+
+	$mjdterms = wp_get_object_terms($mjdids, 'keyword');
+
+?>
 <div class="majors-degrees-search-form">
 	<label for="searchforminput">
 		<h4>Search Majors and Degrees</h4>
 	</label>
-	<form role="search" class="majors-degrees-searchform" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-		<input id="searchforminput" type="text" class="s" name="s" id="s" placeholder="<?php echo $search_terms; ?>" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/><br />
-		<input type="hidden" name="post_type" value="<?php echo get_post_type(); ?>" />
-		<input type="hidden" name="taxonomy" value="keyword" />
+	<form role="search" class="majors-degrees-searchform" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>"">
+		<select id="searchforminput" id="tags">
+			<option value="">Select Keyword</option>
+			<?php
+				foreach ($mjdterms as $key => $value) {
+					echo sprintf('<option value="%s?post_type=majors-and-degrees">%s</option>',
+						get_tag_link($value->term_id),
+						$value->name
+					);
+				}
+			?>
+		</select>
 		<?php do_action('majors_degrees_searchform_fields'); ?>
 	</form>
 </div>
+<script type="text/javascript">
+	document.querySelector('.majors-degrees-search-form #searchforminput').addEventListener('change', function(){
+		document.location.href = this.value;
+	});
+</script>
