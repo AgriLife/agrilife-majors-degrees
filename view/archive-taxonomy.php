3acaf751d2f8bdee187a2b5be31994acf3ac61d5
Zachary Watkins <watkinza@gmail.com>
2018-07-10 16:01:57 -0500

    Improve taxonomy archive page and improve position of heading on single pages

--- view/archive-taxonomy.php
+++ view/archive-taxonomy.php
@@ -30,18 +30,108 @@ get_header();
 ?>
 <div class="content-sidebar-wrap">
 	<main id="genesis-content" class="content">
-		<h1 class="entry-title"><a href="<?php echo get_post_type_archive_link( 'majors-and-degrees' ); ?>">Majors and Degrees tagged "<?php
-		  $taxonomy = get_queried_object();
-		  echo $taxonomy->name;
-	  ?>"</a></h1><?php
+		<h1 class="entry-title"><a href="<?php echo get_post_type_archive_link( 'majors-and-degrees' ); ?>">Majors and Degrees</a></h1><?php
+
+		$taxonomy = get_queried_object();
+
+		$shortcode = sprintf( '[search_form_majors_and_degrees heading="%s" value="%s"]',
+			'Search: ',
+			$taxonomy->name
+		);
+
+	  echo do_shortcode( $shortcode );
 
 		if( have_posts() ) :
 			while( have_posts() ) : the_post();
 				?>
-				<header class="entry-header">
-					<h2>
-						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-					</h2>
+				<div class="about-the-degree"><header class="entry-header">
+					<h2<?php
+
+		        $department_terms = wp_get_post_terms( get_the_ID(), 'department' );
+		        $degree_type_terms = wp_get_post_terms( get_the_ID(), 'degree-type' );
+            $has_meta_terms = !empty($department_terms) || !empty( $degree_type_terms ) ? true : false;
+
+            // If has meta to show below the header, render class name.
+            if( $has_meta_terms ){
+                echo ' class="has-terms"';
+            }
+
+          ?>><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2><?php
+
+            // Show the meta
+            if( $has_meta_terms ){
+
+              ?><div class="meta-terms"><?php
+
+              if( !empty($degree_type_terms) ){
+
+                foreach ($degree_type_terms as $key => $value){
+
+                  $name = $value->name;
+                  $length = count($degree_type_terms);
+                  $space = '';
+
+                  if($key > 0){
+
+                    $space .= $length > 2 ? ', ' : ' ';
+
+                    if($key === $length - 1){
+                      $space .= 'and ';
+                    }
+
+                  }
+
+                  if($key === $length - 1){
+                    $name .= ' Degree';
+                  }
+
+                  $deg = sprintf('%s<span>%s</span>',
+                    $space,
+                    $name
+                  );
+
+                  echo $deg;
+
+                }
+
+              }
+
+              if( !empty($department_terms) ){
+
+                if( !empty( $degree_type_terms ) ){
+
+                  echo ', ';
+
+                }
+
+                foreach ($department_terms as $key => $value){
+
+                  $space = $key < 1 ? '' : ', ';
+
+                  $dept_name = $value->name . ' Department';
+
+                  $link = get_option('taxonomy_' . $value->term_id . '_department-page');
+
+                  if( !empty( $link ) ){
+                    $dept_name = '<a href="' . $link . '">' . $dept_name . '</a>';
+                  }
+
+                  $dept = sprintf('%s<span>%s</span>',
+                    $space,
+                    $dept_name
+                  );
+
+                  echo $dept;
+
+                }
+
+              }
+
+              ?></div><?php
+
+            }
+
+          ?>
 				</header>
 				<div class="entry-content"><?php
 
@@ -54,7 +144,7 @@ get_header();
 					}
 
 				?>
-				</div><?php
+				</div></div><?php
 			endwhile;
 		endif;
 
--- view/shortcode-search-form.php
+++ view/shortcode-search-form.php
@@ -51,10 +51,22 @@
 </script>
 <div class="majors-degrees-search-form">
 	<label for="searchforminput">
-		<h4>Search Majors and Degrees</h4>
+		<h4><?php
+			if($atts && $atts['heading']){
+				echo $atts['heading'];
+			} else {
+				echo 'Search Majors and Degrees';
+			}
+		?></h4>
 	</label>
 	<form role="search" class="majors-degrees-searchform" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>"">
-		<input id="searchforminput" type="text" value=""><?php
+		<input id="searchforminput" type="text" value="<?php
+
+			if($atts && $atts['value']){
+				echo $atts['value'];
+			}
+
+		?>"><?php
 			do_action('majors_degrees_searchform_fields');
 		?>
 		<p class="majors-degrees-search-form-message" style="color:red"></p>
